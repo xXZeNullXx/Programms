0289479c0c0880ee728192f9f51e4cc44a9c8096
xXLeCreeperXx <luca-steffens@web.de>
2023-02-18 17:35:34 +0100

    Added a check for the programs. If the program is installed it hides does not display the button.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import PySimpleGUI as sg
 import subprocess
 
-layout = [[sg.Text("Select Program to install")], [sg.Button("Install 7Zip", key="7Zip")], [sg.Button("Install Brave", key="Brave")]]
+layout = [[sg.Text("Select Program to install")]]
+if subprocess.call(['powershell', 'Get-WindowsPackage -Name 7zip']) != 0:
+    layout.append([sg.Button("Install 7Zip", key="7Zip")])
+if subprocess.call(['powershell', 'Get-WindowsPackage -Name Brave']) != 0:
+    layout.append([sg.Button("Install Brave", key="Brave")])
 
 # Create the window
 window = sg.Window("Program", layout)
@@ -13,10 +17,12 @@ while True:
         # Run the PowerShell command to install 7Zip
         ps_command = 'winget install --id=7zip.7zip  -e'
         subprocess.call(['powershell', '-Command', ps_command])
+        layout.pop(1)
     if event == "Brave":
         # Run the PowerShell command to install Brave
         ps_command = 'winget install --id=Brave.Brave  -e'
         subprocess.call(['powershell', '-Command', ps_command])
+        layout.pop(1)
     if event == sg.WIN_CLOSED:
         break
 
