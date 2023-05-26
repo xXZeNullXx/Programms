625ce18bf44e65ab8bae3980fa9e0864e93060e2
xXLeCreeperXx <luca-steffens@web.de>
2023-05-26 15:07:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,22 +38,18 @@ programs = {
     "flux": "flux.flux",
 }
 
-# Check if the programs are already installed
-ps_command = "winget list"
+# Check if the programs are already installed and remove them from the dictionary
 installed_programs = (
-    subprocess.check_output(["powershell", "-Command", ps_command])
-    .decode()
-    .splitlines()
+    subprocess.check_output(["winget", "list"]).decode().lower().splitlines()
 )
-for program_name in programs.keys():
+for program_name in list(programs.keys()):
     if any(program_name.lower() in program.lower() for program in installed_programs):
-        programs[program_name] = None  # Set to None to hide the button
+        del programs[program_name]
 
 # Create the layout
 layout = [[sg.Text("Select Program to install")]]
-for program_name, program_id in programs.items():
-    if program_id is not None:
-        layout.append([sg.Button(f"Install {program_name}", key=program_name)])
+for program_name in programs.keys():
+    layout.append([sg.Button(f"Install {program_name}", key=program_name)])
 
 # Create the window
 window = sg.Window("Program", layout)
@@ -61,36 +57,23 @@ window = sg.Window("Program", layout)
 # Create an event loop
 while True:
     event, values = window.read()
-    if event in programs.keys() and programs[event] is not None:
+    if event in programs.keys():
         # Run the PowerShell command to install the selected program
         program_id = programs[event]
         ps_command = f"winget install --id={program_id} -e"
         subprocess.call(["powershell", "-Command", ps_command])
 
-        # Re-check if the installed program to update the button status
-        ps_command = "winget list"
-        installed_programs = (
-            subprocess.check_output(["powershell", "-Command", ps_command])
-            .decode()
-            .splitlines()
-        )
-        for program_name in programs.keys():
-            if any(
-                program_name.lower() in program.lower()
-                for program in installed_programs
-            ):
-                programs[program_name] = None
-            else:
-                programs[program_name] = f"{program_name}"
+        # Remove the program from the dictionary
+        del programs[event]
 
         # Update the button status
-        for program_name, program_id in programs.items():
-            if program_id is not None:
-                window[program_name].update(visible=True)
+        for element in window.all_elements():
+            if element.key in programs:
+                element.update(visible=True)
             else:
-                window[program_name].update(visible=False)
+                element.update(visible=False)
 
-    if event == sg.WIN_CLOSED:
+    if event == sg.WINDOW_CLOSED:
         break
 
 window.close()
